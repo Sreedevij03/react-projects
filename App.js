33e2b50493813c384bfb41e7a9064e79c3269d13
Sreedevi Jyothivas <sreedevij03@gmail.com>
2026-02-22 16:32:24 +0530

    chapter 01

---
+++ App.js
@@ -0,0 +1,22 @@
+//Nested html structure in React
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "nexted-child" }, "I am the H1 tag"),
+  ),
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello, Welcome to React!",
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(parent);
